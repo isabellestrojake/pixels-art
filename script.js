fba1d12f190849aee41d9a1c650c56ad54a94255
Isabelle Strojake <strojake@gmail.com>
2022-04-20 17:47:33 -0300

    requisitos 6 e 7

--- script.js
+++ script.js
@@ -3,3 +3,20 @@ backColor[0].style.backgroundColor = 'black';
 backColor[1].style.backgroundColor = '#452966';
 backColor[2].style.backgroundColor = '#518568';
 backColor[3].style.backgroundColor = '#D95B5B';
+
+const preto = document.querySelector('.black');
+const roxo = document.querySelector('.purple');
+const verde = document.querySelector('.green');
+const rosa = document.querySelector('.pink');
+
+function selectedColor(event) {
+  const selec = document.querySelector('.selected');
+  const actual = event.target;
+  selec.classList.remove('selected');
+  actual.classList.add('selected');
+}
+
+preto.addEventListener('click', selectedColor);
+roxo.addEventListener('click', selectedColor);
+verde.addEventListener('click', selectedColor);
+rosa.addEventListener('click', selectedColor);
