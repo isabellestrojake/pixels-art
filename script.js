2fac322d6f1529dfffc426fa06bb567ff7a72357
Isabelle Strojake <strojake@gmail.com>
2022-04-20 16:07:31 -0300

    requisitos 2 e 3

---
+++ script.js
@@ -0,0 +1,5 @@
+const backColor = document.getElementsByClassName('color');
+backColor[0].style.backgroundColor = 'black';
+backColor[1].style.backgroundColor = '#452966';
+backColor[2].style.backgroundColor = '#518568';
+backColor[3].style.backgroundColor = '#D95B5B';
